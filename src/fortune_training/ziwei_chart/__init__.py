193cc7e92b81b801a1d4aad2eec2ed50bce8205b
chinaneedM <china.need.grp@gmail.com>
2026-08-09 23:41:21 +0800

    Export derived-star and role generators

--- src/fortune_training/ziwei_chart/__init__.py
+++ src/fortune_training/ziwei_chart/__init__.py
@@ -1,24 +1,30 @@
 """Deterministic Ziwei chart foundation consuming the shared Time/Calendar layer."""
 
 from .auxiliary import AuxiliaryContext, QSCoreAuxiliaryGenerator
+from .derived_auxiliary import DerivedAuxiliaryGenerator
 from .engine import ZiweiChartFoundation, ZiweiChartRequest
 from .main_stars import MainStarGenerator
-from .models import NatalChartState, NatalStructureState, Sex
+from .models import NatalChartState, NatalStructureState, RoleBinding, Sex
 from .natal import NatalStructureGenerator, NatalStructureInput
 from .profile import ResolvedZiweiCalculationProfile
+from .roles import QSRoleGenerator, WenmoDefaultRoleGenerator
 
 __all__ = [
     "AuxiliaryContext",
+    "DerivedAuxiliaryGenerator",
     "MainStarGenerator",
     "NatalChartState",
     "NatalStructureGenerator",
     "NatalStructureInput",
     "NatalStructureState",
     "QSCoreAuxiliaryGenerator",
+    "QSRoleGenerator",
     "ResolvedZiweiCalculationProfile",
+    "RoleBinding",
     "Sex",
+    "WenmoDefaultRoleGenerator",
     "ZiweiChartFoundation",
     "ZiweiChartRequest",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
