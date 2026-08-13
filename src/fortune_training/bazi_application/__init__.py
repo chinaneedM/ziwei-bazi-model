d1ce96fa3e557ded29b7b00c48a589dc8a525263
chinaneedM <china.need.grp@gmail.com>
2026-08-13 15:44:14 +0800

    feat: export Bazi application integrity API

--- src/fortune_training/bazi_application/__init__.py
+++ src/fortune_training/bazi_application/__init__.py
@@ -1,7 +1,9 @@
 """Local productization layer for released Bazi natal and Dayun facts."""
 
+from .integrity import validate_application_resolution
 from .models import (
     BaziApplicationCandidate,
+    BaziApplicationIntegrityReport,
     BaziApplicationRequest,
     BaziApplicationResolution,
 )
@@ -10,12 +12,14 @@ from .service import BaziApplicationResolutionError, BaziChartService
 
 __all__ = [
     "BaziApplicationCandidate",
+    "BaziApplicationIntegrityReport",
     "BaziApplicationProfile",
     "BaziApplicationRequest",
     "BaziApplicationResolution",
     "BaziApplicationResolutionError",
     "BaziChartService",
     "bazi_local_application_v1_profile",
+    "validate_application_resolution",
 ]
 
 __version__ = "0.1.0"
